ec60fdbaeb072e0491031323f3ea18635d9238a1
PranayVJain <pranayjain000@gmail.com>
2019-01-18 18:40:13 +0530

    Test webhook by adding issue

--- backend/src/test/java/com/messenger/chatterbox/backend/CheckBoxMainTest.java
+++ backend/src/test/java/com/messenger/chatterbox/backend/CheckBoxMainTest.java
@@ -1,12 +1,13 @@
 package com.messenger.chatterbox.backend;
 
-
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 /**
  * Unit test for simple App.
  */
 public class CheckBoxMainTest {
 
 	public static void main(String a[]){
-		
+		InputStream i = new ByteArrayInputStream(null);
 	}
 }
